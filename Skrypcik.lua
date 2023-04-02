521b081b40773564b5555dd76b124b44d87b0242
FastDust11 <38609216+FastDust11@users.noreply.github.com>
2023-04-02 22:54:18 +0200

    Create Skrypcik.lua

---
+++ Skrypcik.lua
@@ -0,0 +1,36 @@
+wyniki = {}
+-- Reakcja na zniszczenie samolotu przeciwnika
+--EVENTHANDLER={}
+
+deduwa = EVENTHANDLER:New()
+deduwa:HandleEvent(EVENTS.Kill)
+
+function deduwa:OnEventKill(EventData)
+    gracz = EventData.IniPlayerName
+    MESSAGE:New(" gracz = " .. EventData.IniPlayerName .. " ", 20):ToAll()
+
+
+    --"20" czas wyświetlania wiadomości
+    if wyniki[gracz] == nil then
+        wyniki[gracz] = 1
+    else
+        wyniki[gracz] = wyniki[gracz] + 1
+    end
+    MESSAGE:New(" gracz wynik = " .. wyniki[gracz] .. " ", 20):ToAll()
+end
+
+podrabiana_deduwa = EVENTHANDLER:New()
+podrabiana_deduwa:HandleEvent(EVENTS.PlayerEnterAircraft)
+
+function podrabiana_deduwa:OnEventPlayerEnterAircraft(EventData)
+    gracz = EventData.IniPlayerName
+    MESSAGE:New(" gracz śmierć = " .. EventData.IniPlayerName .. " ", 20):ToAll()
+    Wyniki[gracz] = nil
+end
+
+--Do zrobienia:
+--1. Warunek dla eventhandlera czy koles nie zginal (reset pentli)
+--2. Ma zliczac tylko samoloty
+--3. Pomijanie pierwszego killa (liczy do 5 dla kadzego inny komunikat na 6 Nagroda)
+--4. Dodanie dzwiekow
+--5. Nagroda za serie zabojstw
